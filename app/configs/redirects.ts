1a95231ccae17d885cab5d64b8ef0d6ad89d9939
M Haidar Hanif <git@mhaidarhanif.com>
2023-10-04 22:11:58 +0700

    fix: config redirects

--- app/configs/redirects.ts
+++ app/configs/redirects.ts
@@ -1,6 +1,9 @@
 export const configRedirects = [
   { path: "/", url: "https://gdg.community.dev/gdg-bandung" },
   { path: "/sponsor", url: "https://forms.gle/fihWMesmfcR1M96f9" },
-  { path: "/cfs ", url: "https://sessionize.com/devfest-bandung-2023" },
-  { path: "/devfest ", url: "https://gdg.community.dev/events/details/google-gdg-bandung-presents-devfest-bandung-2023/" },
+  { path: "/cfs", url: "https://sessionize.com/devfest-bandung-2023" },
+  {
+    path: "/devfest",
+    url: "https://gdg.community.dev/events/details/google-gdg-bandung-presents-devfest-bandung-2023/",
+  },
 ];
--- app/utils/route-redirect.ts
+++ app/utils/route-redirect.ts
@@ -4,9 +4,9 @@ import { configRedirects } from "~/configs";
 
 export function redirectRouteToURL(request: Request) {
   const url = new URL(request.url);
-  const foundItem = configRedirects.find(
-    (item) => item.path === url.pathname
-  );
+  const foundItem = configRedirects.find((item) => {
+    return item.path.trim() === url.pathname;
+  });
 
   if (!foundItem) return json(null);
   return redirect(foundItem?.url);
