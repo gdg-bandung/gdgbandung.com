b971b5a366991052ff3580b4740c680f0da27a3c
unknown <fannanff25@gmail.com>
2024-09-30 14:14:30 +0700

    feat: updating cfs

--- app/configs/redirects.ts
+++ app/configs/redirects.ts
@@ -1,7 +1,11 @@
 export const configRedirects = [
   { path: "/", url: "https://gdg.community.dev/gdg-bandung" },
   { path: "/sponsor", url: "https://forms.gle/fihWMesmfcR1M96f9" },
-  { path: "/cfs", url: "https://sessionize.com/devfest-bandung-2023" },
+  // { path: "/cfs", url: "https://sessionize.com/devfest-bandung-2023" },
+  {
+    path : "/cfs",
+    url : "https://sessionize.com/devfest-bandung-2024/"
+  },
   {
     path: "/devfest23",
     url: "https://gdg.community.dev/events/details/google-gdg-bandung-presents-devfest-bandung-2023/",
