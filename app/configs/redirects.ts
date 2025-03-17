fa45284e9a9e60d408f0f2fc23fee73646844f50
fan <fannanff25@gmail.com>
2025-03-17 15:41:13 +0700

    test: bsb8

--- app/configs/redirects.ts
+++ app/configs/redirects.ts
@@ -304,6 +304,18 @@ export const configRedirects = [
   },
   {
     path: "/bsb8",
-    url: "https://drive.google.com/file/d/13yKuBoq1_sXugbEwpFWldD-gh4yNufX5/view?usp=sharing",
+    url: "https://forms.gle/12iy6hLPM5mgaU9r6",
+  },
+  {
+    path: "/bsb9",
+    url: "https://forms.gle/2uSU3MLLKphZjGod8",
+  },
+  {
+    path: "/bsb8-feedback",
+    url: "https://forms.gle/6Nu6rG8WeG1iVVEW9",
+  },
+  {
+    path: "/bsb9-feedback",
+    url: "https://forms.gle/PxKofpbkYWj44maUA",
   },
 ];
