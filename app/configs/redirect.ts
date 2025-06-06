11715e5b9232362f0e6d6ba443d65eea5fd39771
Hadian Rahmat <hadian.rahmat@gmail.com>
2025-06-06 10:05:44 +0700

    feat: add enpoint sponsorship devfest

--- app/configs/redirect.ts
+++ app/configs/redirect.ts
@@ -2,6 +2,9 @@ export const configRedirects = [
   {
     path: "/iox25",
     url: "https://gdg.community.dev/events/details/google-gdg-bandung-presents-google-io-extended-bandung-2025/",
+  },{
+    path: "/devfest-sponsorship",
+    url: "https://docs.google.com/presentation/d/1Pb1F7yha3wOfBcUDm0AAmluzwBF-MQwnWLMfTX8PyJo/edit?usp=sharing",
   },
   // TODO: Add redirect for devfest25
   //   {
