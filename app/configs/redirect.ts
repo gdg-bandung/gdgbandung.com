a1758f4bf25b15618b398288d4f8dc5c831e2466
Hadian Rahmat <hadian.rahmat@gmail.com>
2025-06-10 15:49:47 +0700

    feat: add endpoint for ToR speaker

--- app/configs/redirect.ts
+++ app/configs/redirect.ts
@@ -2,9 +2,14 @@ export const configRedirects = [
   {
     path: "/iox25",
     url: "https://gdg.community.dev/events/details/google-gdg-bandung-presents-google-io-extended-bandung-2025/",
-  },{
+  },
+  // DevFest Bandung 2025
+  {
     path: "/devfest-sponsorship",
     url: "https://docs.google.com/presentation/d/1Pb1F7yha3wOfBcUDm0AAmluzwBF-MQwnWLMfTX8PyJo/edit?usp=sharing",
+  },{
+    path: "/devfest-tor",
+    url: "https://docs.google.com/document/d/1SoQmDa-d9NNUxbVw35eAqxdokker_FUDq3jQvCm0I6g/edit?usp=sharing",
   },
   // TODO: Add redirect for devfest25
   //   {
